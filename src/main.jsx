5f708208478bcecd4ad9da3762c848352608db1e
sannma31 <kasaisyouun2004@gmail.com>
2023-09-03 20:39:42 +0900

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const shincondeInfo = {
+  name:"shincode",
+  age: 24,
+};
+
+const ShinCodetext = createContext(shincondeInfo);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ShinCodetext.Provider value ={shincondeInfo}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </ShinCodetext.Provider>
 )
